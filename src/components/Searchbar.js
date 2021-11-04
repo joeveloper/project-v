5788175f8dc95d2c6ca42ba85a6b08a851143037
joeveloper <bebosir@gmail.com>
2021-11-04 16:42:59 +0100

    Initial commit

---
+++ src/components/Searchbar.js
@@ -0,0 +1,10 @@
+const Searchbar = () => {
+    return ( 
+        <form action="">
+        <input type="text" />
+        <button>search</button>
+      </form>
+     );
+}
+ 
+export default Searchbar;
